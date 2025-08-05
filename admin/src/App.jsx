53e2127eba66be5a426233cc3f24d6dfa2f6e5ba
agnivo988 <agnivamukherjee977@gmail.com>
2025-08-05 23:19:48 +0530

    updated front and backend links

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from 'react-toastify';
 const App = () => {
 
 
-   const url = "http://localhost:4000"
+   const url = "https://foody-backend-bua3.onrender.com"
 
   return (
     <div>
