f81920b4790f9b9ee4359e30665c1022b1e7c988
Suvam Chatterjee <suvamchatterjee873@gmail.com>
2025-08-30 09:54:42 +0530

    FIx App Download Sec

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -4,7 +4,7 @@ import { assets } from '../../assets/assets'
 const AppDownload = () => {
   return (
     <div className='app-download' id='app-download'>
-      <h1>Download the app now!</h1> <br /><p>Experience seamless online ordering
+      <h2>Download the app now!</h2> <br /><p>Experience seamless online ordering
 only on the Foody app </p>
         <div className="app-download-platforms">
             <img src={assets.play_store} alt="" />
